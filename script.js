2ed859c14e9d1125fbf3b49a1a1e18e379057233
Cukrzyk05 <oskarfon05@gmail.com>
2025-04-29 02:33:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// Logowanie i Rejestracja
 document.querySelector('.login-btn').addEventListener('click', function() {
     alert('Formularz logowania');
-    // Można dodać wyskakujące okno z formularzem logowania
 });
 
 document.querySelector('.register-btn').addEventListener('click', function() {
     alert('Formularz rejestracji');
-    // Można dodać wyskakujące okno z formularzem rejestracji
+});
+
+// Zmiana motywu
+const themeToggleButton = document.getElementById('theme-toggle');
+themeToggleButton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    // Zmieniamy ikonę przycisku
+    themeToggleButton.textContent = document.body.classList.contains('dark-mode') ? '🌞' : '🌙';
 });
