a90c7fe2c801b5ed26bc44b3dbeb077dc7dff836
Cukrzyk05 <oskarfon05@gmail.com>
2025-04-29 03:51:17 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = 'https://your-project.supabase.co'; // <- zmień na swój URL
-const SUPABASE_ANON_KEY = 'your-anon-public-key'; // <- zmień na swój ANON KEY
+const SUPABASE_URL = 'https://vnlpafhoowdvxfhxzhpf.supabase.co'; // <- zmień na swój URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZubHBhZmhvb3dkdnhmaHh6aHBmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU4OTExODQsImV4cCI6MjA2MTQ2NzE4NH0.f_NoMRFCD6hak7l-r1eUBQKTl5Bn9acsdHw5MGugAKQ'; // <- zmień na swój ANON KEY
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
