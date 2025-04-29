47c097e6dd305f8c1ac562eb4cf005dc79d63810
Cukrzyk05 <oskarfon05@gmail.com>
2025-04-29 17:37:34 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://aihkjtyhzvwhnzaovoil.supabase.co'; // <- Zastąp własnym URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpaGtqdHloenZ3aG56YW92b2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MzE4NDAsImV4cCI6MjA2MTUwNzg0MH0.Of960rPQcjf3e2TZAza3gwf_6hzSXxsC7AtNXUsmGPw'; // <- Zastąp własnym kluczem z dashboardu
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
